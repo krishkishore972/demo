f04f5495f61cefe9834469a4cfcba27e2f1b7db2
krishkishore972 <krishkishore972@gmail.com>
2025-08-29 15:45:27 +0530

    made some change

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
+        THIS IS FOR TESTING PURPOSES
       </p>
     </>
   )
